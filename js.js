273fc0d5182c807c295d4f52e2819e03e74819c7
Luciano Petillo <luciano.prnl@gmail.com>
2022-07-25 13:23:10 +0200

    add javaScript listener

--- js.js
+++ js.js
@@ -1,2 +1,16 @@
 const boxes = document.querySelectorAll('.box');
 
+function toggleOpen(){
+  console.log("Hello");
+  this.classList.toggle('open');
+}
+
+function toggleActive(e){
+  console.log(e.propertyName);
+  if (e.propertyName.includes('flex')) {
+    this.classList.toggle('open-active');
+  }
+}
+
+boxes.forEach(box => box.addEventListener('click', toggleOpen));
+boxes.forEach(box => box.addEventListener('transitionend', toggleActive));
